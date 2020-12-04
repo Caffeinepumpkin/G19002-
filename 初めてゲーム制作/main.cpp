079f4eb32c6023ee312df231f5d937bf25708880
kakebutoxun <hj-g19002@sist.ac.jp>
2020-12-04 09:47:06 +0900

    作品制作gitに追加

---
+++ 初めてゲーム制作/main.cpp
@@ -0,0 +1,9 @@
+#include "DxLib.h"
+#include "resource.h"
+
+#define GAME_WIDTH	800	//画面の横の大きさ
+#define GAME_HEIGHT	600	//画面の縦の大きさ
+#define GAME_COLOR	32	//画面のカラービット
+
+#define GAME_WINDOW_BAR	0	
+#define GAME_WINDOW_NAME	"GAME TITLE"
